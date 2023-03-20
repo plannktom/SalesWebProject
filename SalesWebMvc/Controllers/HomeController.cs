536f00e30cc6d22d2c126d84198ea966b664017f
Paulo Stefam Viana <plannktom@gmail.com>
2023-03-20 12:33:42 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.Viewmodels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SalesWebMvc.Models
+namespace SalesWebMvc.Models.Viewmodels
 {
     public class ErrorViewModel
     {
