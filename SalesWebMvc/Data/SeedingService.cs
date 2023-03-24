ad116b113713daf86e84d2aa71a03dabc26f0888
Paulo Stefam Viana <plannktom@gmail.com>
2023-03-24 17:23:02 -0300

    SellersController

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.Enums;
 
 namespace SalesWebMvc.Data
 {
@@ -26,9 +27,9 @@ namespace SalesWebMvc.Data
                 return; //Banco de Dados Já Foi Populado, Não fazer nada
             }
 
-            Department d1 = new Department(1,"GERAL");
-            Seller s1 = new Seller(1,"GERAL",null,new DateTime(2000,01,01),0.0,d1);
-            SalesRecord r1 = new SalesRecord(1, new DateTime(2000, 01, 01),0.0,Models.Enums.SaleStatus.Billed,s1);
+            Department d1 = new Department(1,"Geral");
+            Seller s1 = new Seller(1, "Geral", null,new DateTime(2000,01,01),0.0,d1);
+            SalesRecord r1 = new SalesRecord(1, new DateTime(2000, 01, 01),0.0,SaleStatus.Billed,s1);
 
             _context.Department.AddRange(d1);
             _context.Seller.AddRange(s1);
